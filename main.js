d00845344964e956ef5d36dbeee11f87eedd62dd
shamiaw8 <shamiaw8@gmail.com>
2026-06-27 00:42:19 -0400

    Update main.js

--- main.js
+++ main.js
@@ -11,22 +11,11 @@ function updateClock() {
 }
 
 async function startApp() {
-  try {
-    await requireUser();
-    setupLogout();
-    updateClock();
-    setInterval(updateClock, 1000);
-  } catch (error) {
-    document.body.innerHTML = `
-      <main class="app">
-        <section class="room">
-          <h1 class="room-title">something went sideways</h1>
-          <p>${error.message}</p>
-          <a class="back-link" href="login.html">go to login</a>
-        </section>
-      </main>
-    `;
-  }
+  await requireUser("login.html");
+  setupLogout("logoutBtn");
+
+  updateClock();
+  setInterval(updateClock, 1000);
 }
 
 startApp();
