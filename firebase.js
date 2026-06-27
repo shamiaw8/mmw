c707ca68de5348af1a06554f0dbe7f9cbf8dd75b
shamiaw8 <shamiaw8@gmail.com>
2026-06-27 00:12:19 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
-
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
-
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -17,5 +15,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-
 export const db = getFirestore(app);
