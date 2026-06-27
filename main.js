d41e1c1fbe6cd843f632e4363cd3a8c18fa74fd5
shamiaw8 <shamiaw8@gmail.com>
2026-06-27 00:04:03 -0400

    Update main.js

--- main.js
+++ main.js
@@ -10,9 +10,12 @@ function updateClock() {
   });
 }
 
-await requireUser("login.html");
-setupLogout();
+try {
+  await requireUser("login.html");
+  setupLogout();
 
-updateClock();
-setInterval(updateClock, 1000);
-
+  updateClock();
+  setInterval(updateClock, 1000);
+} catch (error) {
+  console.error("mmw startup error:", error);
+}
